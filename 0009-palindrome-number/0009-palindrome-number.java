ad546133f6e16de52ab2d041254ed96bcbe533f6
Namasivayen <namasivayenns@gmail.com>
2026-01-23 10:25:12 +0530

    Time: 5 ms (73.45%), Space: 46 MB (42.48%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,13 +1,10 @@
 class Solution {
     public boolean isPalindrome(int x) {
         int r = 0; int t = x;
-        if(x<0){
-            return false;
-        }
-        while(t!=0){
+        while(t>0){
             r = r*10 + t%10;
             t/=10;
         }
-        return (r==x);
+        return (x<0)?false:(r==x);
     }
 }
